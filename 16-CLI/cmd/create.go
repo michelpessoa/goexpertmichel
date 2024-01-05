155ae91e11ecd1fe986131e115d07665ac3468c2
Michel Pessoa <michelpessoa@gmail.com>
2024-01-05 20:59:11 -0300

    CLI init

---
+++ 16-CLI/cmd/create.go
@@ -0,0 +1,64 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"github.com/michelpessoa/goexpertmichel/16-CLI/internal/database"
+	"github.com/spf13/cobra"
+)
+
+// createCmd represents the create command
+// var createCmd = &cobra.Command{
+// 	Use:   "create",
+// 	Short: "A brief description of your command",
+// 	Long: `A longer description that spans multiple lines and likely contains examples
+// and usage of using your command. For example:
+
+// Cobra is a CLI library for Go that empowers applications.
+// This application is a tool to generate the needed files
+// to quickly create a Cobra application.`,
+// 	Run: func(cmd *cobra.Command, args []string) {
+// 		fmt.Println("create called")
+// 	},
+// }
+
+func newCreateCmd(categoryDb database.Category) *cobra.Command {
+	return &cobra.Command{
+		Use:   "create",
+		Short: "Create a new category",
+		Long:  `Create a new category`,
+		RunE:  runCreate(categoryDb),
+	}
+}
+
+func runCreate(categoryDb database.Category) RunEFunc {
+	return func(cmd *cobra.Command, args []string) error {
+		name, _ := cmd.Flags().GetString("name")
+		description, _ := cmd.Flags().GetString("description")
+		_, err := categoryDb.Create(name, description)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+}
+
+func init() {
+	createCmd := newCreateCmd(GetCategoryDB(GetDb()))
+
+	categoryCmd.AddCommand(createCmd)
+	createCmd.Flags().StringP("name", "n", "", "Name of the category")
+	createCmd.Flags().StringP("description", "d", "", "Description of the category")
+	createCmd.MarkFlagsRequiredTogether("name", "description")
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// createCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ 16-CLI/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/michelpessoa/goexpertmichel/16-CLI/cmd"
+
+func main() {
+	cmd.Execute()
+}
