d14e7111cc514879782a5176ee5a9113fe7f8f88
Michel Pessoa Keursten <michel@DESKTOP-G354PKN>
2024-01-10 22:51:27 -0300

    Injeção de dependência com WIRE

---
+++ 19-DI/wire.go
@@ -0,0 +1,25 @@
+//go:build wireinject
+// +build wireinject
+
+package main
+
+import (
+	"database/sql"
+
+	"github.com/michelpessoa/goexpert/19-DI/product"
+
+	"github.com/google/wire"
+)
+
+var setRepositoryDependency = wire.NewSet(
+	product.NewProductRepository,
+	wire.Bind(new(product.ProductRepositoryInterface), new(*product.ProductRepository)),
+)
+
+func NewUseCase(db *sql.DB) *product.ProductUseCase {
+	wire.Build(
+		setRepositoryDependency,
+		product.NewProductUseCase,
+	)
+	return &product.ProductUseCase{}
+}
